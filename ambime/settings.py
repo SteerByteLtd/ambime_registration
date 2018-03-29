d4a6eec556dd323b6a6d4063ad0cc122b7ff9696
Ubuntu <ubuntu@ip-172-31-31-147.eu-west-2.compute.internal>
2018-03-29 17:08:42 +0000

    Update SECRET_KEY variable in settings.py

--- ambime/settings.py
+++ ambime/settings.py
@@ -1,14 +1,6 @@
 import os
-from django.core.exceptions import ImproperlyConfigured
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-
-def get_env_variable(name):
-    try:
-	return os.environ[name]
-    except KeyError:
-	error_msg = "Set the %s environment variable" % name
-	raise ImproperlyConfigured(error_msg)
-
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
@@ -32,7 +24,7 @@ DEBUG = False
 
 WSGI_APPLICATION = 'ambime.wsgi.application'
 
-SECRET_KEY = get_env_variable('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'ambime_secret')
 
 SITE_ID = 1
 
